50111e674a4f147c8493cd1e7aff1e960d359174
Hasan-Saju <56477243+Hasan-Saju@users.noreply.github.com>
2021-07-20 13:35:34 +0600

    Class Components

--- React_Basics/hello-world/src/App.js
+++ React_Basics/hello-world/src/App.js
@@ -1,32 +1,48 @@
-import React from 'react';
+import React,{Component} from 'react';
 import './App.css';
+import Person from './components/Person'
 
 
 // JSX
-function App() {
-  // return (
-  //   <div className="App">
-  //    <h2>Hello Ecfor</h2>
-  //    <Person/>
-  //    {/* <Person></Person> */}
-  //   </div>
-  // );
+// Functional Component
+// function App() {
+//   return (
+//     <div className="App">
+//      <h2>Hello Ecfor</h2>
+//      <Person/>
+//      // <Person></Person> 
+//     </div>
+//   );
 
-  return React.createElement('div',{className:"App"},React.createElement('h1',null,"Hello World"),<Person/>);
-  // React.createElement('tag',attribute,vitore ja thakbe)
-}
+//   // return React.createElement('div',{className:"App"},React.createElement('h1',null,"Hello World"),<Person/>);
+//   // React.createElement('tag',attribute,vitore ja thakbe)
+// }
 
 // class attribute in html is className in jsx
 // only 1 root element is possible in jsx
 
-function Person() {
-  // return(
-  //   <div>
-  //     <h3>I am person component.</h3>
-  //   </div>
-  // );
-
-  return React.createElement('div',null,React.createElement('h1',null,"I am person component"));
+// Class component
+class App extends Component{
+  // calculation can be done
+  render(){
+    return (
+          <div className="App">
+           <h2>Hello Ecfor</h2>
+           <Person/>
+          </div>
+        );
+  }
 }
+
+
+// function Person() {
+//   return(
+//     <div>
+//       <h3>I am person component.</h3>
+//     </div>
+//   );
+
+//   // return React.createElement('div',null,React.createElement('h1',null,"I am person component"));
+// }
 
 export default App;
---
+++ React_Basics/hello-world/src/components/Person.js
@@ -0,0 +1,11 @@
+import React from 'react'; //for compiling jsx in Person()
+
+function Person() {
+  return(
+    <div>
+      <h3>I am person component.</h3>
+    </div>
+  );
+}
+
+export default Person;
