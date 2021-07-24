a8116c966a53f00951e4e9c3e9a0e35bbdf3d777
Hasan-Saju <hasansaju11@gmail.com>
2021-07-24 22:49:53 +0600

    Setting up React Router Packages

--- React_Router/hello-world/src/components/lists/BookList.js
+++ React_Router/hello-world/src/components/lists/BookList.js
@@ -1,25 +1,18 @@
-import React, { Component } from "react";
+import React from "react";
 import Book from "../representational/Book";
 
-class BookList extends Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    console.log("BookList render");
-    return this.props.books.map((book, index) => {
-      return (
-        <Book
-          bookName={book.bookName}
-          writer={book.writer}
-          delete={() => this.props.deleteBookState(index)}
-          key={book.id}
-          inputName={(event) => this.props.changeWithInputState(event, index)}
-        />
-      );
-    });
-  }
-}
+const BookList = (props) => {
+  return props.books.map((book, index) => {
+    return (
+      <Book
+        bookName={book.bookName}
+        writer={book.writer}
+        delete={() => props.deleteBookState(index)}
+        key={book.id}
+        inputName={(event) => props.changeWithInputState(event, index)}
+      />
+    );
+  });
+};
 
 export default BookList;
