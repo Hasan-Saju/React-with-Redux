490209b4602c9ecd272951dc1f091900aa332f2a
Hasan-Saju <hasansaju11@gmail.com>
2021-07-24 22:24:34 +0600

    Ready for React Router

---
+++ React_Router/hello-world/src/assets/books.js
@@ -0,0 +1,7 @@
+const books = [
+  { id: 1, bookName: "Deyal", writer: "Humayun" },
+  { id: 2, bookName: "Lal Nil", writer: "Humayun Azad" },
+  { id: 3, bookName: "BD", writer: "1971" },
+];
+
+export default books;
---
+++ React_Router/hello-world/src/components/lists/BookList.js
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+import Book from "../representational/Book";
+
+class BookList extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    console.log("BookList render");
+    return this.props.books.map((book, index) => {
+      return (
+        <Book
+          bookName={book.bookName}
+          writer={book.writer}
+          delete={() => this.props.deleteBookState(index)}
+          key={book.id}
+          inputName={(event) => this.props.changeWithInputState(event, index)}
+        />
+      );
+    });
+  }
+}
+
+export default BookList;
