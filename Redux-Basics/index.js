298f30e075e60f14365ac490dc70ef649280cb7d
Hasan-Saju <hasansaju11@gmail.com>
2021-07-27 22:58:56 +0600

    Dispatching Actions

--- Redux-Basics/index.js
+++ Redux-Basics/index.js
@@ -9,10 +9,33 @@ const initState = {
 
 // Reducer
 const rootReducer = (state = initState, action) => {
-  return state;
+  if (action.type === "INC_NUM") {
+    return {
+      ...state,
+      num: state.num + 1,
+    };
+  }
+  if (action.type === "ADD_NUM") {
+    return {
+      ...state,
+      num: state.num + action.value,
+    };
+  }
 };
 
 // Store
 const store = redux.createStore(rootReducer);
+
+// console.log(store.getState());
+
+// Dispatching Action
+store.dispatch({
+  type: "INC_NUM", //everything will be uppercase while defining type
+});
+
+store.dispatch({
+  type: "ADD_NUM",
+  value: 34,
+});
 
 console.log(store.getState());
